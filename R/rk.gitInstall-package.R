2a8039a6b83264bc94ba0469f40fadf7fab59e7a
unDocUMeantIt <m@reaktanz.de>
2015-12-18 19:20:56 +0100

    prep for release
      - added ChangeLog and some meta files
      - debianized the package
      - update build script and rebuilt

---
+++ R/rk.gitInstall-package.R
@@ -0,0 +1,23 @@
+#' RKWard GUI to install R packges directly from git or subversion repositories.
+#'
+#' \tabular{ll}{
+#' Package: \tab rk.gitInstall\cr
+#' Type: \tab Package\cr
+#' Version: \tab 0.01-0\cr
+#' Date: \tab 2015-12-18\cr
+#' Depends: \tab  rkward (>= 0.6.0)\cr
+#' Enhances: \tab rkward\cr
+#' License: \tab GPL (>= 3)\cr
+#' LazyLoad: \tab yes\cr
+#' URL: \tab https://rkward.kde.org\cr
+#' }
+#'
+#' RKWard GUI to install R packges directly from git or subversion repositories
+#'
+#' @aliases rk.gitInstall-package
+#' @name rk.gitInstall-package
+#' @docType package
+#' @title The rk.gitInstall Package
+#' @author Meik Michalke
+#' @keywords package
+NULL
